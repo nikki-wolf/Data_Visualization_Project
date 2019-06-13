771fa8dbe30754e4f95bf7125b57a86b58f07fad
nikki-wolf <beygi.mr@gmail.com>
2019-06-13 05:13:52 -0500

    added GeoJson API to app.py

--- app_files/app.py
+++ app_files/app.py
@@ -11,6 +11,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import inspect
 import json
 from bson import ObjectId
+from collections import OrderedDict
 
 
 import pymongo
@@ -36,67 +37,58 @@ client = pymongo.MongoClient(conn_mlab)
 db = client.heroku_stp5z9b7
 
 # create/read collections for options 1 and 2:
-wine_opt1=db.wine # creating/reading collection by option 1
-wine_opt2=db.wine_history # creating/reading collection by option 2   
+wine_opt1=db.wine_rating # creating/reading collection by option 1
+wine_opt2=db.wine_history # creating/reading collection by option 2  
+wine_opt2_list=db.wine_history_list # creating/reading collection by option 2  
 
 func = lambda s: s[:1].lower() + s[1:] if s else '' #function to return lower case of all character of a strign
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route("/api")
-def jdata():
-    #try:
-    wd=wine_opt2.find({},{'_id': False})
+@app.route("/api_rating")
+def idata():
+    wd=wine_opt1.find({},{'_id': False})
     rows=[]
     for data in wd:
         rows.append(data)
 
-    #json_rows=json.dumps(rows)
-    #except Exception ,e:
-    #print str(e)
-
     return jsonify(rows)
 
+@app.route("/api_history")
+def jdata():
+    wd=wine_opt2.find({},{'_id': False})
+    rows=[]
+    for data in wd:
+        rows.append(data)
+    return jsonify(rows)
 
-@app.route("/api/country=<country>")
+@app.route("/api_history/country=<country>")
 def jdata_country(country):
     country=func(country).title()
-    #try:
+
     wd=wine_opt2.find({'country': country}, {'_id': False})
     rows=[]
     for data in wd:
         rows.append(data)
 
-    #json_rows=json.dumps(rows)
-    #except Exception ,e:
-    #print str(e)
-
     return jsonify(rows)
 
-@app.route("/api/year=<year>")
-#@app.route("/api/year=<year>&country=")
+@app.route("/api_history/year=<year>")
 def jdata_year(year):
-    #try:
     wd=wine_opt2.find({'year': year}, {'_id': False})
     rows=[]
     for data in wd:
         rows.append(data)
 
-    #json_rows=json.dumps(rows)
-#except Exception ,e:
-#    print str(e)
-
     return jsonify(rows)
-@app.route("/api/country=<country>&year=<int:year>")
+
+@app.route("/api_history/country=<country>&year=<int:year>")
 def jdata_country_year(country='',year=''):
-    #try:
-    print("country=",country,"year=",year)
-    print(request.base_url)
+    country=func(country).title()
     if (year):
         print("contry and year")
         wd=wine_opt2.find({'country': country,'year': str(year)}, {'_id': False})
@@ -108,15 +100,10 @@ def jdata_country_year(country='',year=''):
     for data in wd:
         rows.append(data)
 
-    #json_rows=json.dumps(rows)
-#except Exception ,e:
-#    print str(e)
-
     return jsonify(rows)
 
-@app.route("/api/startyear=<syear>&endyear=<eyear>")
+@app.route("/api_history/startyear=<syear>&endyear=<eyear>")
 def jdata_styear_enyear(syear,eyear):
-    #try:
     wd=wine_opt2.find({'$and':[
         {'year': {'$gte': syear}}, 
         {'year': {'$lte': eyear}}
@@ -126,15 +113,11 @@ def jdata_styear_enyear(syear,eyear):
     for data in wd:
         rows.append(data)
 
-    #json_rows=json.dumps(rows)
-#except Exception ,e:
-#    print str(e)
-
     return jsonify(rows)
 
-@app.route("/api/country=<country>&startyear=<syear>&endyear=<eyear>")
+@app.route("/api_history/country=<country>&startyear=<syear>&endyear=<eyear>")
 def jdata_country_styear_enyear(country,syear,eyear):
-    #try:
+    country=func(country).title()
     wd=wine_opt2.find({'$and':[
         {'year': {'$gte': syear}}, 
         {'year': {'$lte': eyear}},
@@ -144,16 +127,91 @@ def jdata_country_styear_enyear(country,syear,eyear):
     for data in wd:
         rows.append(data)
 
-    #json_rows=json.dumps(rows)
-#except Exception ,e:
-#    print str(e)
-
     return jsonify(rows)
-#@app.route("/production")
-#def production():
-#    # return jsonify(df.to_json(orient="records"))
-#    # return jsonify(df.to_json(orient="columns"))
-#    return jsonify(production_df.to_json(orient="table"))
+
+@app.route("/api_history/GeoJSON")
+def jdata_geojson():
+    wd=wine_opt2_list.find({},{'_id': False})
+    rows=[]
+    for data in wd:
+        rows.append(data)
+
+    geojson = {
+        "type": "FeatureCollection",
+        "metadata": {
+        "title": "Wine History by Pygeons",
+        "status": 200,
+        "count": len(rows)
+        },
+        "features": [
+        {
+            "type": "Feature",
+
+            "geometry" : {
+                "type": "Point",
+                "coordinates": [d["coordinate"]["lon"], d["coordinate"]["lat"]],
+            },
+
+            "properties" : 
+            {
+
+              "country":d['country'],
+              "year": d['year'],
+              "Production":  d["Production"],
+              "Production_capita": d["Production_capita"],
+              "Export": d["Export"],
+              "Import": d["Import"],
+              "Consumption": d["Consumption"],
+              "Consumption_capita": d["Consumption_capita"],
+              "Population": d["Population"],     
+            } 
+        }  for d in rows]
+    }
+    return json.dumps(geojson)
+
+
+@app.route("/api_history/GeoJSON/country=<country>")
+def jdata_geojson_country(country):
+    country=func(country).title()
+    wd=wine_opt2_list.find({'country': country}, {'_id': False})
+    rows=[]
+    for data in wd:
+        rows.append(data)
+
+    geojson = {
+        "type": "FeatureCollection",
+        "metadata": {
+        "title": "Wine History by Pygeons",
+        "status": 200,
+        "count": len(rows)
+        },
+        "features": [
+        {
+            "type": "Feature",
+
+            "geometry" : {
+                "type": "Point",
+                "coordinates": [d["coordinate"]["lon"], d["coordinate"]["lat"]],
+            },
+
+            "properties" : 
+            {
+
+              "country":d['country'],
+              "year": d['year'],
+              "Production":  d["Production"],
+              "Production_capita": d["Production_capita"],
+              "Export": d["Export"],
+              "Import": d["Import"],
+              "Consumption": d["Consumption"],
+              "Consumption_capita": d["Consumption_capita"],
+              "Population": d["Population"],     
+            } 
+        }  for d in rows]
+    }
+    return json.dumps(geojson)
+
+
 
 if __name__ == "__main__":
     app.run()

c13a8a1c634a89179706dc9877db48381b3498ba
Heather <heathertrinh1017@gmail.com>
2019-06-22 01:49:00 -0500

    ui

--- app_files/static/js/heather.js
+++ app_files/static/js/heather.js
@@ -1,3 +1,4 @@
+// global variables
 var countryUnpack
 var priceUnpack
 var ratingUnpack
@@ -5,7 +6,7 @@ var varietyUnpack
 var uniqueCountry
 var uniqueID = [];
 
-
+// plot function
 function myParallel() {
   var url = "/api_rating_extended";
   d3.json(url).then(function(wineData) {
@@ -28,6 +29,19 @@ function myParallel() {
         varietyUnpack = unpack(wineData, 'variety')
         console.log(varietyUnpack, 'unpacked variety')
 
+        // wineData.forEach(function(data){
+        //   countries = JSON.parse(data.Country)
+        //   console.log(countries, “parsed countries”)
+        //   price = JSON.parse(data.price.replace(/\bnan\b/g, "null"))
+        //   console.log(price, 'parsed price')
+        //   rating = JSON.parse(data.rating.replace(/\bnan\b/g, "null"))
+        //   console.log(rating, 'parsed rating')
+        //   varieties = eval(‘(’+data.Variety+‘)’)})
+        //   console.log(varieties, “parsed varieties”)
+
+
+    
+
         // retrieve unique values of countries
         uniqueCountry = countryUnpack.filter((v, i, a) => a.indexOf(v) === i);
         console.log(uniqueCountry);
@@ -42,57 +56,56 @@ function myParallel() {
         });
         
      
-      var data = [{
-        type: 'parcoords',
-        visible: true,
-        line: {
-          showscale: true,
-          colorscale: 'Jet',
-          cmin: 1,
-          cmax: 3000,
-          color:  uniqueID
-        },
-       
-        dimensions: [{
-          range: [0,43],
-          tickvals: uniqueID,
-          ticktext: uniqueCountry,
-          label: 'Countries',
-          values: uniqueID
-        }, {
-          constraintrange: [2000,2500],
-          range: [Math.min(...priceUnpack),
-          Math.max(...priceUnpack)],
-          range:[0,10],
-          label: 'Price',
-          values: priceUnpack,
-          multiselect:true
-        
-        }, {
-          constraintrange: [86,90],
-          multiselect: true,
-          label: 'Rating',
-          range: [80,
-          Math.max(...ratingUnpack)],
-          range: [0,10],
-          values: ratingUnpack
-        }, {
-          label: 'Variety',
-          range: [0,6],
-          tickvals: Array.apply(0,new Array(6)).map(function(_,i){ return i+1 }),
-          ticktext: varietyUnpack,
-          values: varietyUnpack
+        var data = [{
+          type: 'parcoords',
+          visible: true,
+          line: {
+            // color: unpack(wineData, 'country'),
+            color:  Array.apply(0,new Array(43)).map(function(_,i){ return i+1 }),
+            colorscale: 'Electric',
+            cmin:1,
+            cmax: 42,
+            values: uniqueID
+          },
+         
+          dimensions: [{
+            range: [0,43],
+            tickvals: uniqueID,
+            ticktext: uniqueCountry,
+            label: 'Countries',
+            values: uniqueCountry
+          }, {
+            constraintrange: [2000,2500],
+            range: [Math.min(...priceUnpack),
+            Math.max(...priceUnpack)],
+            label: 'Price',
+            values: priceUnpack,
+            multiselect:true
+          
+          }, {
+            constraintrange: [86,90],
+            multiselect: true,
+            label: 'Rating',
+            range: [80,
+            Math.max(...ratingUnpack)],
+            values: ratingUnpack
+          }, {
+            range: [0,6],
+            label: 'Variety',
+            tickvals: Array.apply(0,new Array(6)).map(function(_,i){ return i+1 }),
+            ticktext: varietyUnpack,
+            values: varietyUnpack
+      
+          }
+          ]
+        }];
+        var layout = {
+          width: 710,
+          height: 620
+        };
     
-        }
-        ]
-      }];
-      var layout = {
-        width: 710,
-        height: 620
-      };
-  
-    
-      Plotly.plot('heatherDiv', data, layout);
-    });
-  }
+      
+        Plotly.plot('heatherDiv', data, layout);
+      });
+    }
 myParallel()
